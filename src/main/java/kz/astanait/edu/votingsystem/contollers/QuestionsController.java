36f581fc9a93dfd02bd56986f95e8fa887c759a0
nurymsiyrbayev <nurym.kazah@gmail.com>
2021-03-06 01:49:56 +0600

    Added functionality User statistic by Interests, Fixed some issues from htmls

--- src/main/java/kz/astanait/edu/votingsystem/contollers/QuestionsController.java
+++ src/main/java/kz/astanait/edu/votingsystem/contollers/QuestionsController.java
@@ -1,6 +1,7 @@
 package kz.astanait.edu.votingsystem.contollers;
 
 import kz.astanait.edu.votingsystem.exceptions.UserNotFoundException;
+import kz.astanait.edu.votingsystem.models.Interest;
 import kz.astanait.edu.votingsystem.models.Option;
 import kz.astanait.edu.votingsystem.models.Question;
 import kz.astanait.edu.votingsystem.models.User;
@@ -18,6 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -79,8 +81,8 @@ public class QuestionsController {
         return "redirect:/home?success";
     }
 
-    @GetMapping("/full-statistics")
-    public String getFullStatistics(@RequestParam("question") Question question, Model model, Principal principal) {
+    @GetMapping("/full-statistics/{id}")
+    public String getFullStatistics(@PathVariable("id") Question question, Model model, Principal principal) {
         User user = userService.findUserByNickname(principal.getName());
         List<User> groupMates = userService.findUsersByGroup(user.getGroup());
         Map<Option, List<User>> usersVotedOptions = new LinkedHashMap<>();
@@ -99,12 +101,79 @@ public class QuestionsController {
                     .collect(Collectors.toList())
                     .forEach(vote -> sameVotedUsers.add(vote.getUser()));
 
-            if (!sameVotedUsers.isEmpty())
+            if (!sameVotedUsers.isEmpty()) {
                 usersVotedOptions.put(option, sameVotedUsers);
+            }
         });
 
+//Interest
+/*
+        Map<Option, Map<Interest, List<User>>> usersCommonInterest = new LinkedHashMap<>();
+
+        Set<Interest> sortedInterests = user.getInterests().stream()
+                .sorted(Comparator.comparingLong(Interest::getId))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+        user.setInterests(sortedInterests);
+
+//        Set<User> usersWithSameInterest = new LinkedHashSet<>();
+
+        user.getInterests().forEach(interest -> {
+            List<User> usersWithSameInterest = userService.findUsersByInterestsContains(interest);
+                question.getOptions().forEach(option -> {
+                    List<User> sameVotedUsers = new ArrayList<>();
+                    List<Vote> votes = voteService.findVotesByQuestionAndOption(question, option);
+
+                    votes.stream()
+                            .filter(vote -> usersWithSameInterest.contains(vote.getUser()))
+                            .collect(Collectors.toList())
+                            .forEach(vote -> sameVotedUsers.add(vote.getUser()));
+
+                    if (!sameVotedUsers.isEmpty()) {
+                        Map<Interest, List<User>> interestListMap = new LinkedHashMap<>();
+                        interestListMap.put(interest, sameVotedUsers);
+                        usersCommonInterest.put(option, interestListMap);
+                    }
+                });
+        });
+*/
+
+        // Block with for every option, find All users, sharing same Interest
+        List<Interest> allInterest = interestService.findAll();
+        Map<Option, Map<Interest, Set<User>>> usersCommonInterest = new LinkedHashMap<>();
+        // For every Option of Question
+        for (Option option : question.getOptions()) {
+            // If option has 1 or less vote, then no need to process this option
+            if (option.getVoteCount() <= 1) {
+                continue;
+            }
+            Map<Interest, Set<User>> sameInterestUsers = new LinkedHashMap<>();
+            // Find votes for every Option
+            List<Vote> votes = voteService.findVotesByQuestionAndOption(question, option);
+            // For every Interest
+            for (Interest interest : allInterest) {
+                // If this interest have 1 or less users, exit loop
+                if (interest.getUsers().size() <= 1) {
+                    continue;
+                }
+                Set<User> commonInterestUsers = new LinkedHashSet<>();
+                for (Vote vote : votes) {
+                    // Check, if voted User have this Interest
+                    if (vote.getUser().getInterests().contains(interest)) {
+                        commonInterestUsers.add(vote.getUser());
+                    }
+                }
+                if (commonInterestUsers.size() > 1) {
+                    sameInterestUsers.put(interest, commonInterestUsers);
+                }
+            }
+            usersCommonInterest.put(option, sameInterestUsers);
+        }
+        // Block with for every option, find All users, sharing same Interest
+
+        log.info(String.valueOf(usersCommonInterest));
         model.addAttribute("question", question);
-        model.addAttribute("statistics", usersVotedOptions);
+        model.addAttribute("statisticsByGroupMates", usersVotedOptions);
+        model.addAttribute("statisticsByInterests", usersCommonInterest);
         return "questions/full-statistics";
     }
 }
